f118414803835135a0c59688e7527e204154e0af
Darren <darren.daley@hotmail.co.uk>
2025-07-24 17:43:55 +0100

    got it up and working !

---
+++ MoonworksTutorial/RenderColoredTriangle/ColoredTriangleGame.cs
@@ -0,0 +1,27 @@
+﻿using MoonWorks;
+using MoonWorks.Graphics;
+
+namespace RenderColoredTriangle;
+public class ColoredTriangleGame : Game
+{
+    public ColoredTriangleGame(
+       AppInfo appInfo,
+       WindowCreateInfo windowCreateInfo,
+       FramePacingSettings framePacingSettings
+       ) : base(
+           appInfo,
+           windowCreateInfo,
+           framePacingSettings,
+           ShaderFormat.SPIRV | ShaderFormat.DXIL | ShaderFormat.MSL | ShaderFormat.DXBC)
+    {
+        ShaderCross.Initialize();
+    }
+
+    protected override void Update(TimeSpan delta)
+    {
+    }
+
+    protected override void Draw(double alpha)
+    {
+    }
+}
