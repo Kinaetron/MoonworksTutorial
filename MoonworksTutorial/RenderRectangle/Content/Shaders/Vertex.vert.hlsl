92026ede319900dfeee1a5496c0536f58ad5c629
Darren <darren.daley@hotmail.co.uk>
2025-07-25 11:11:57 +0100

    rectangle done !

---
+++ MoonworksTutorial/RenderRectangle/Content/Shaders/Vertex.vert.hlsl
@@ -0,0 +1,16 @@
+﻿struct Input
+{
+    float2 Position : POSITION;
+};
+
+struct Output
+{
+    float4 Position : SV_Position;
+};
+
+Output main(Input input)
+{
+    Output output;
+    output.Position = float4(input.Position, 0.0f, 1.0f);
+    return output;
+}
---
+++ MoonworksTutorial/RenderRectangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using RenderRectangle;
+using MoonWorks;
+
+internal class Program
+{
+    static void Main()
+    {
+        var windowCreateInfo = new WindowCreateInfo(
+             "Rectangle Example Game",
+             800,
+             600,
+             ScreenMode.Windowed
+         );
+
+        var framePacingSettings = FramePacingSettings.CreateLatencyOptimized(60);
+
+        var game = new BasicRectangleGame(
+            new AppInfo("Rectangle Example Game", "RectangleExampleGame"),
+            windowCreateInfo,
+            framePacingSettings);
+
+        game.Run();
+    }
+}
